20da9a133d186a1bd30aed710199e056173cc822
yenushka1999 <kalindiyenushka@gmail.com>
2025-12-15 15:46:29 +0530

    Fix logout redirect instead of JSON response

--- auth/logout.php
+++ auth/logout.php
@@ -1,6 +1,8 @@
 <?php
 require_once __DIR__ . '/../functions.php';
+
 session_unset();
 session_destroy();
-header('Content-Type: application/json; charset=utf-8');
-echo json_encode(['ok'=>true]);
+
+header("Location: ../index.php");
+exit;
